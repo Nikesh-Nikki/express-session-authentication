5a827bf36e57ddb15d12aa99980743537dfe1ed9
nikesh <nikesh.tadela@gmail.com>
2024-01-29 23:16:51 +0530

    just basic session cookie implimentation

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import session from "express-session";
+import pgconnect from "connect-pg-simple";
+
+const pgSession=pgconnect(session);
+const pool = new pg.Pool(
+  {
+      host: 'localhost',
+      user: 'postgres',
+      password : 'Nikesh@123',
+      database : "secrets",
+      port : 5432,
+      max: 20,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+  }
+);
 
 const db=new pg.Client({
   host : "localhost",
@@ -15,10 +31,28 @@ db.connect();
 const app = express();
 const port = 3000;
 
+app.use(session({
+  store: new pgSession({
+    pool : pool,                // Connection pool
+    createTableIfMissing : true,
+    tableName : 'user_sessions'   // Use another table-name than the default "session" one
+    // Insert connect-pg-simple options here
+  }),
+  secret: 'process.env.FOO_COOKIE_SECRET',
+  resave: false,
+  saveUninitialized : true,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  if(req.session.viewCount){
+    req.session.viewCount++;
+  }else{
+    req.session.viewCount=1;
+  }
+  console.log(req.session.viewCount);
   res.render("home.ejs");
 });
 
